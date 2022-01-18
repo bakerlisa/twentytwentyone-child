b20f09e55bc3619ae27de901266f2d2c1774e889
Lisa Broadhead <37422388+lisabroadhead@users.noreply.github.com>
2022-01-18 11:24:06 -0600

    Add files via upload

---
+++ template-parts/content/content-none.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<section class="no-results not-found">
+
+	<div class="page-content">
+			<p>Sorry, but there are no posts to display at this time. Enter a new search below to find what you are looking for.</p>
+			<?php get_search_form(); ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
